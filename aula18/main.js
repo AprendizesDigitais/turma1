730b331295c3e3970c6dbcefe682260b1f1f0e4d
GabrielSegatto <gabriel.segatto58@gmail.com>
2018-11-05 13:44:22 -0200

    fix bugs aula18

--- aula18/main.js
+++ aula18/main.js
@@ -2,7 +2,7 @@ function ampliaEsq(div){
     console.log('Ampliou!');
     div.style.transform = 'scale(2,2)';
     div.style.transition = "2s";
-    div.style.zIndex = "10";
+    div.style.zIndex = "11";
     div.style.transformOrigin = "center left";
 }
 
@@ -25,4 +25,5 @@ function encolhe(div){
     console.log('Encolheu!');
     div.style.transform = 'scale(1,1)';
     div.style.transition = "2s";
+    div.style.zIndex = "-1";
 }
