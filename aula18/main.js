55795866851325bb71519ab0454b91bf17c80440
GabrielSegatto <gabriel.segatto58@gmail.com>
2018-11-05 13:09:54 -0200

    add a aula 18

---
+++ aula18/main.js
@@ -0,0 +1,28 @@
+function ampliaEsq(div){    
+    console.log('Ampliou!');
+    div.style.transform = 'scale(2,2)';
+    div.style.transition = "2s";
+    div.style.zIndex = "10";
+    div.style.transformOrigin = "center left";
+}
+
+function ampliaCent(div){    
+    console.log('Ampliou!');
+    div.style.transform = 'scale(2,2)';
+    div.style.transition = "2s";
+    div.style.zIndex = "10";
+}
+
+function ampliaDir(div){    
+    console.log('Ampliou!');
+    div.style.transform = 'scale(2,2)';
+    div.style.transition = "2s";
+    div.style.zIndex = "10";
+    div.style.transformOrigin = "center right";
+}
+
+function encolhe(div){
+    console.log('Encolheu!');
+    div.style.transform = 'scale(1,1)';
+    div.style.transition = "2s";
+}
